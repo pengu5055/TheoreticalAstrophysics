a63ba45e7993cf76c352e95e0bb432ffddfdcadd
pengu5055 <urbancmarko1@gmail.com>
2021-12-30 21:49:10 +0100

    Determination of model parameters

--- DN1/main_01.py
+++ DN1/main_01.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import odeint
+from scipy.integrate import odeint, quad
 import cmasher as cmr
 from src_methods import *
 import matplotlib.colors as color
@@ -18,6 +18,42 @@ def lane_emden(theta, xi):
     Returns second derivative
     """
     return theta[1], -(theta[0]**n + 2/xi*theta[1])
+
+
+def xi_to_r(xi, xi1):
+    """Convert from dimensionless xi to dimensionless r/R"""
+    return xi/xi1
+
+
+def rho_c(xi1, dtheta):
+    M = -4
+
+
+def density_norm(theta, n):
+    """Returns density normalised to central density \rho/\rho_c"""
+    return theta**n
+
+
+def pressure_norm(theta, n):
+    """Returns P= K\rho_c^{1 + 1/n}\theta^{n+1} but with constants set to 1"""
+    return theta**(n+1)
+
+
+def temperature_norm(theta, n):
+    r"""Returns T = K\mu/k_B \rho_C^{1/n} \theta with scale constant K an mean molecular weight set to 1"""
+    k_b = 1.38 * 10**-23
+    return k_b*theta
+
+
+def mass_norm(theta, n, xi1):
+    M = quad(lambda xi, theta: xi**2 * theta**n, 0, xi1, args=(theta,))  # Whole mass without constants
+    G = 6.67408 * 10**-11
+    output = []
+    for t in theta:
+        func = lambda t, n: 4 * np.pi * G * density_norm(t, n)
+        output.append(quad(func, 0, r, args=(n,)))
+
+    return np.array(output)
 
 
 # Test plot
@@ -52,24 +88,24 @@ def lane_emden(theta, xi):
 # plt.show()
 
 # Plot derivatives
-n_list = [0, 0.1, 0.5, 1, 1.5, 3, 5]
-theta_init = [1, 0]
-xi_range = np.linspace(0.01, 10, 1000000)
-colors = cmr.take_cmap_colors("cmr.cosmic", 7, cmap_range=(0.15, 1), return_fmt="hex")
+# n_list = [0, 0.1, 0.5, 1, 1.5, 3, 5]
+# theta_init = [1, 0]
+# xi_range = np.linspace(0.01, 10, 1000000)
+# colors = cmr.take_cmap_colors("cmr.cosmic", 7, cmap_range=(0.15, 1), return_fmt="hex")
 
 
-for index, i in enumerate(n_list):
-    n = i
-    sol = np.column_stack(odeint(lane_emden, theta_init, xi_range))
-    plt.plot(xi_range, sol[1], label="n = {}".format(i), c=colors[index])
+# for index, i in enumerate(n_list):
+#     n = i
+#     sol = np.column_stack(odeint(lane_emden, theta_init, xi_range))
+#     plt.plot(xi_range, sol[1], label="n = {}".format(i), c=colors[index])
 
-plt.title("Odvod Lane-Emdenove enačbe")
-plt.xlabel(r"$\xi$")
-plt.ylabel(r"$\theta$")
-plt.legend()
-plt.axhline(alpha=1, ls=":", c="#adadad")
-plt.ylim(-0.8, 0.2)
-plt.show()
+# plt.title("Odvod Lane-Emdenove enačbe")
+# plt.xlabel(r"$\xi$")
+# plt.ylabel(r"$\theta$")
+# plt.legend()
+# plt.axhline(alpha=1, ls=":", c="#adadad")
+# plt.ylim(-0.8, 0.2)
+# plt.show()
 
 # Continuous version
 # div = 200
@@ -92,4 +128,22 @@ plt.show()
 # plt.colorbar(plt.cm.ScalarMappable(cmap=winter, norm=color.Normalize(vmin=0, vmax=5)),
 #              label="Politropni indeks")
 # plt.show()
+
+# Density, Mass, Temperature, Pressure plots n = 1.5 (monoatomic gas)
+n = 1.5
+xi1 = 3.64
+theta_init = [1, 0]
+xi_range = np.linspace(0.01, xi1, 50000)
+theta, dtheta = np.column_stack(odeint(lane_emden, theta_init, xi_range))
+r = xi_to_r(xi_range, xi1)
+
+plt.plot(r, theta)
+plt.show()
+
+# Density plot
+rho = density_norm(theta, n)
+plt.plot(r, rho)
+plt.show()
+
+# Mass plot
 
